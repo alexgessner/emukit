220bfde8ef2d6eb1406d01f0de3d8cc16cde6e19
agessner <agessner@tue.mpg.de>
2019-09-30 19:39:48 +0200

    started working on derivatives

--- emukit/bayesian_optimization/pmin_hdr.py
+++ emukit/bayesian_optimization/pmin_hdr.py
@@ -29,7 +29,7 @@ def joint_min(mu: np.ndarray, var: np.ndarray, with_derivatives: bool=False) -> 
 
 
 class ProbMinLoop():
-    def __init__(self, mu: np.ndarray, Sigma: np.ndarray, with_derivatives: bool, N_subset: int = 8,
+    def __init__(self, mu: np.ndarray, Sigma: np.ndarray, with_derivatives: bool, N_subset: int = 16,
                  N_hdr: int = 1024):
         """
         Computes the approximate probability of every of the N representer points to be the minimum.
@@ -115,23 +115,26 @@ class ProbMinSingle():
         self.lincon = cgi.LinearConstraints(np.dot(M, self.L), np.dot(M, self.mu))
 
         # subset simulation
-        self.subsetsim = cgi.subset_simulation.SubsetSimulation(self.lincon, self.N_subset, 0.5, n_skip=3)
+        self.subsetsim = cgi.subset_simulation.SubsetSimulation(self.lincon, self.N_subset, 0.5, n_skip=9)
         self.subsetsim.run_loop(verbose=False)
 
         # set up HDR
         self.hdr = cgi.hdr.HDR(self.lincon, self.subsetsim.tracker.shifts(), self.N_hdr,
-                               self.subsetsim.tracker.x_inits(), n_skip=3) # TODO: surface n_skip
+                               self.subsetsim.tracker.x_inits(), n_skip=2) # TODO: surface n_skip
 
         if self.with_derivatives:
             self.first_moment = None
             self.second_moment = None
+
+        # store samples when computing the integral, and the integral
+        self.X_samples = None
 
     def log_pmin(self):
         """
         Compute the logarithm of the approximate integral for p_min using HDR
         :return: np.float log of pmin
         """
-        self.hdr.run(verbose=False)
+        self.X_samples = self.hdr.run(verbose=False)
         return self.hdr.tracker.log_integral()
 
     def samples(self, N_samples):
@@ -140,6 +143,13 @@ class ProbMinSingle():
         :param N_samples: number of samples drawn
         :return: samples (np.ndarray)
         """
+        # ESS from domain
+        ess = cgi.loop.EllipticalSliceOuterLoop(N_samples - self.X_samples.shape[-1],
+                                                self.lincon, n_skip=2, x_init=self.X_samples)
+        ess.run_loop()
+
+        # Now these samples are drawn in the whitened space, so they need to be back-transformed
+        return np.dot(self.L, ess.loop_state.X) + self.mu
 
     def get_moments(self, f_samples):
         """
@@ -147,26 +157,48 @@ class ProbMinSingle():
         :param f_samples: samples from the integrand
         :return: first moment of f (np.ndarray)
         """
-        pass
+        return self.get_sample_mean(f_samples), self.get_sample_cov(f_samples)
 
-    def dlogPdMu(self):
+    def get_sample_mean(self, f_samples):
+        """
+        Computes the first moment of f w.r.t. the integrand of pmin
+        :param f_samples: samples from the integrand
+        :return: first moment of f (np.ndarray)
+        """
+        return np.mean(f_samples, axis=1)
+
+    def get_sample_cov(self, f_samples):
+        """
+        Computes the second moment of f w.r.t. the integrand of pmin
+        :param f_samples: samples from the integrand
+        :return: second moment of f (np.ndarray)
+        """
+
+    def dlogPdMu(self, f_samples):
         """
         Gradient of logP for every representer point w.r.t. mu
+        :param f_samples: samples from the integrand
         :return: with gradient, np.ndarray, dim (N)
         """
-        pass
+        f_mu_mean = self.get_sample_mean(f_samples-self.mu)
+        Sigmainv_fm = slinalg.solve_triangular(self.L.T, slinalg.solve_triangular(self.L, f_mu_mean, lower=True),
+                                               lower=False)
+        return Sigmainv_fm / self.hdr.tracker.integral()
 
-    def dlogPdSigma(self):
+
+    def dlogPdSigma(self, f_samples):
         """
         Gradient of logP for every representer point w.r.t. Sigma.
         Since Sigma is symmetric, only N(N+1)/2 values need to be stored per representer point
+        :param f_samples: samples from the integrand
         :return: (N(N+1)/2,) np.ndarray with gradient
         """
         pass
 
-    def dlogPdMudMu(self):
+    def dlogPdMudMu(self, f_samples):
         """
         Hessian w.r.t. mean mu
+        :param f_samples: samples from the integrand
         :return: Hessian, dim (N,N)
         """
         pass
