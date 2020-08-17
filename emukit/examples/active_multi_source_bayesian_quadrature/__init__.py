7cb5972e1f3e53075639db95c2890433123505bb
agessner <agessner@tue.mpg.de>
2020-08-17 13:58:14 +0200

    fixed some more imports

--- emukit/examples/active_multi_source_bayesian_quadrature/__init__.py
+++ emukit/examples/active_multi_source_bayesian_quadrature/__init__.py
@@ -1,9 +1,9 @@
 # Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-from . import multi_source_bq
-from . import integral_bounds
-from . import example_functions
+from .multi_source_bq import MultiSourceBayesianQuadrature
+from .integral_bounds import IntegralBounds
 from . import acquisition
 from . import kernels
 from . import loop
+from . import example_functions
